c0de984c559074ab04453cb83f343e7d99f84f20
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2024-12-03 22:23:33 +0200

    Finished Firebase Sign In, Login and LogOut tutorial

--- src/navigations/Routes.tsx
+++ src/navigations/Routes.tsx
@@ -1,25 +1,60 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home, Login } from './Screens/Index';
+import Details from './Screens/Details';
+import List from './Screens/List';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from '../FirebaseConfig';
 
 export type ScreenType ={
     Home: any;
     Login: any;
+    Inside : any;
+    Details : any;
 }
 const Stack = createNativeStackNavigator<ScreenType>();
+
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout()
+{
+  return(
+    <InsideStack.Navigator>
+      <InsideStack.Screen name = "My todos" component={List}/>
+      <InsideStack.Screen name = "details" component={Details}/>
+    </InsideStack.Navigator>
+  )
+}
 const Routes = () => {
+
+  const [user,setUser] = useState<User | null>(null);
+
+useEffect(() =>{
+  onAuthStateChanged(FIREBASE_AUTH, (user)=>{
+    console.log('user', user)
+    setUser(user);
+  })
+},[])
   return (
     <NavigationContainer>
         <Stack.Navigator 
             screenOptions={{headerShown:false}}
             initialRouteName='Login'>
-            <Stack.Screen name ={'Login'} component ={Login}/>
+              {user ? (
+                <Stack.Screen name ="Inside" component ={InsideLayout}
+                options = {{headerShown: false}}/>  
+              ) : (
+                <Stack.Screen name ="Login" component ={Login}
+                options = {{headerShown: false}}/>               
+              )}
+            {/*<Stack.Screen name ={'Login'} component ={Login}/>*/}
             <Stack.Screen name={"Home"} component={Home}/> 
         </Stack.Navigator>
     </NavigationContainer>
   )
 }
 
-export default Routes
+export default  Routes
+
